e5f34ceeb7658e4b76eacb499c4e21c7e9a3762d
veenith123 <72656950+veenith123@users.noreply.github.com>
2022-01-01 14:00:20 +0530

    Update Alphabet-Recognition.py

--- Alphabet-Recognition.py
+++ Alphabet-Recognition.py
@@ -1,4 +1,3 @@
-#Importing all the important models and install them if not installed on your device
 import cv2
 import numpy as np
 import pandas as pd
@@ -47,40 +46,29 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=9, train_
 X_train_scaled = X_train/255.0
 X_test_scaled = X_test/255.0
 
-#Fitting the training data into the model
 clf = LogisticRegression(solver='saga', multi_class='multinomial').fit(X_train_scaled, y_train)
 
-#Calculating the accuracy of the model
 y_pred = clf.predict(X_test_scaled)
 accuracy = accuracy_score(y_test, y_pred)
 print("The accuracy is :- ",accuracy)
 
-#Starting the camera
 cap = cv2.VideoCapture(0)
 
 while(True):
-  # Capture frame-by-frame
   try:
     ret, frame = cap.read()
 
-    # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     
-    #Drawing a box in the center of the video
     height, width = gray.shape
     upper_left = (int(width / 2 - 56), int(height / 2 - 56))
     bottom_right = (int(width / 2 + 56), int(height / 2 + 56))
     cv2.rectangle(gray, upper_left, bottom_right, (0, 255, 0), 2)
 
-    #To only consider the area inside the box for detecting the digit
-    #roi = Region Of Interest
     roi = gray[upper_left[1]:bottom_right[1], upper_left[0]:bottom_right[0]]
 
-    #Converting cv2 image to pil format
     im_pil = Image.fromarray(roi)
 
-    # convert to grayscale image - 'L' format means each pixel is 
-    # represented by a single value from 0 to 255
     image_bw = im_pil.convert('L')
     image_bw_resized = image_bw.resize((28,28), Image.ANTIALIAS)
 
@@ -94,13 +82,10 @@ while(True):
     test_pred = clf.predict(test_sample)
     print("Predicted class is: ", test_pred)
 
-    # Display the resulting frame
     cv2.imshow('frame',gray)
     if cv2.waitKey(1) & 0xFF == ord('q'):
       break
   except Exception as e:
     pass
-
-# When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
